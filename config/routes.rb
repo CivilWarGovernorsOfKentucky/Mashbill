284318bd1e321810834344801cc6ad811d527e34
saracarl <saracarl@gmail.com>
2016-08-18 09:14:04 -0500

    dashboard route

--- config/routes.rb
+++ config/routes.rb
@@ -63,6 +63,6 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-  get   '/dashboard' => 'users#dashboard'
+  get   '/dashboard' => 'users#dashboard', as: :dashboard
 
 end
