3ed010a875ad89545efd091c5104004b755cf76f
Ben W. Brumfield <benwbrum@gmail.com>
2016-08-28 08:27:00 -0500

    Root route goes to user dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  root 'users#dashboard'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
