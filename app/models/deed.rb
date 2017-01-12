9195385f1163072af610941b96833ec709831182
saracarl <saracarl@gmail.com>
2017-01-12 14:23:01 -0600

    restricted deed types for #8

--- app/models/deed.rb
+++ app/models/deed.rb
@@ -1,6 +1,37 @@
 class Deed < ActiveRecord::Base
+# constants
+  DOC_COMPLETED = 'doc_completed'
+  ENTITY_EDIT = 'entity_edit'
+  ENTITY_CREATE = 'entity_create'
+  ENTITY_DELETE = 'entity_delete'
+  RELATIONSHIP_EDIT = 'relationship_edit'
+  RELATIONSHIP_CREATE = 'relationship_create'
+  RELATIONSHIP_DELETE = 'relationship_delete'
+
   belongs_to :user
   belongs_to :document
   belongs_to :entity
   belongs_to :relationship
+
+  validates_inclusion_of :deed_type, :in => [ DOC_COMPLETED, ENTITY_DELETE, ENTITY_CREATE, ENTITY_EDIT, RELATIONSHIP_DELETE, RELATIONSHIP_CREATE, RELATIONSHIP_EDIT ]
+
+  def deed_type_name
+    return case self.deed_type
+    when DOC_COMPLETED
+      'Document Completed'
+    when ENTITY_EDIT
+      'Entity Edited'
+    when ENTITY_CREATE
+      'Entity Created'
+    when ENTITY_DELETE
+      'Entity Deleted'
+    when RELATIONSHIP_EDIT
+      'Relationship Edited'
+    when RELATIONSHIP_CREATE
+      'Relationship Created'
+    when RELATIONSHIP_DELETE
+      'Relationship Deleted'
+    end
+  end
+
 end
