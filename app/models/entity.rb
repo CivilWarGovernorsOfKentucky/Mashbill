42dd71049bf4cd6ac2121ca1153371fa32b4c3c7
saracarl <saracarl@gmail.com>
2017-08-01 09:08:11 -0500

    references need to be prefixed cwgk for #61

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -57,7 +57,8 @@ class Entity < ActiveRecord::Base
 
   def ref_id
     if id != nil 
-      self[:ref_id] || Type::REF_PREFIX[entity_type] + id.to_s.rjust(8,'0')
+      raw_ref_id = self[:ref_id] || Type::REF_PREFIX[entity_type] + id.to_s.rjust(8,'0')
+      "cwgk:" + raw_ref_id
     end
   end
 
